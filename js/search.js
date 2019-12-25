c67137040e915e1db36e80c2ed872e532b41b09d
Loic <loic.caron@isen.yncrea.fr>
2019-12-25 16:52:38 +0100

    Fonction recherche de plat fonctionnelle

--- js/search.js
+++ js/search.js
@@ -1,4 +1,24 @@
 let search = (function(){
+       
+    var input = document.getElementById('searchbar');
 
-    
+    input.addEventListener('keyup', event => {
+        
+        let string = input.value.toUpperCase();
+        let tab = document.getElementById('tab')
+        let len = tab.getElementsByTagName('tr').length;
+
+        for(let i = 1; i < len; i++){
+            
+           tab.deleteRow(1);
+        }
+
+        for(let i = 0; i < plats.length; i++){
+            
+            if(((plat.name(i)).toUpperCase().indexOf(string)) != -1){
+
+                displayLine.outDisplayLine(i);
+            }
+        }
+    });
 })();
